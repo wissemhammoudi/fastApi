47c616d30410a8e3df3f7e65bf75adb607695f23
wissemhammoudi <hammoudiwissem81@gmail.com>
2025-02-06 16:06:21 +0100

    adding new endpoint

--- routers/question.py
+++ routers/question.py
@@ -6,6 +6,12 @@ from database import db_dependency
 # Create router instance for question-related endpoints
 router = APIRouter( prefix="/question",tags=["questions"])
 
+
+@router.get("/")
+async def get_questions(db: db_dependency):
+    result = db.query(Question).all()
+    return result
+
 @router.post("/")
 async def create_question(question: QuestionBase, db: db_dependency):
     """
@@ -32,8 +38,8 @@ async def create_question(question: QuestionBase, db: db_dependency):
         db.add(db_choise)
     db.commit()
 
-@router.get("/{question_id}")
-async def get_question(question_id: int, db: db_dependency):
+@router.get("/{question_text}")
+async def get_question(question_text: str, db: db_dependency):
     """
     Get a specific question by ID
     Args:
@@ -45,11 +51,31 @@ async def get_question(question_id: int, db: db_dependency):
         HTTPException: If question is not found
     """
     # Query database for question matching the ID
-    result = db.query(Question).filter(Question.id == question_id).first()
+    result = db.query(Question).filter(Question.question_text.ilike(f"%{question_text}%")).limit(5).all()
     # Raise 404 if question not found
     if not result:
         raise HTTPException(status_code=404, detail="Question not found")
     return result
+@router.delete("/{question_id}")
+async def delete_question(question_id: int, db: db_dependency):
+    result = db.query(Question).filter(Question.id == question_id).first()
+    choices = db.query(Choice).filter(Choice.question_id == question_id).all()
+    if not result:
+        raise HTTPException(status_code=404, detail="Question not found")
+    for choice in choices:
+        db.delete(choice)
+    db.delete(result)
+    db.commit()
+
+@router.patch("/{question_id}")
+async def update_question(question_id: int, question: str, db: db_dependency):
+    result = db.query(Question).filter(Question.id == question_id).first()
+    if not result:
+        raise HTTPException(status_code=404, detail="Question not found")
+    result.question_text = question
+    db.commit()
+
+
 
 
 
